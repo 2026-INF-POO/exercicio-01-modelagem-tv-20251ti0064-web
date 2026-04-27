12712ef7a2577c8580a4eb7f5e0564b002c17aa5
20251ti0064-web <20251ti0064@ifba.edu.br>
2026-04-27 14:41:55 -0300

    Update TV.java

--- TV.java
+++ TV.java
@@ -1,1 +1,38 @@
+vpublic class TV {
 
+    int tamanhoTela;
+    int volume = 5;
+    String marca;
+    int voltagem;
+    int canal;
+
+    void ligar() {
+        int consumo = voltagem * tamanhoTela;
+        System.out.println("TV ligada");
+        System.out.println("Consumo: " + consumo);
+    }
+
+    void desligar() {
+        System.out.println("TV desligada");
+    }
+
+    void aumentarVolume() {
+        volume++;
+        System.out.println("Volume: " + volume);
+    }
+
+    void diminuirVolume() {
+        volume--;
+        System.out.println("Volume: " + volume);
+    }
+
+    void subirCanal() {
+        canal++;
+        System.out.println("Canal: " + canal);
+    }
+
+    void descerCanal() {
+        canal--;
+        System.out.println("Canal: " + canal);
+    }
+}
